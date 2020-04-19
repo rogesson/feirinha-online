3f1ef2f606ea56c6f4f348bd2c0bf54bf73cac39
hugopassos <hugo.passos@hotmail.com>
2020-04-19 16:35:28 -0300

    Fixed query in Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where(id: UserRole.where(user_id: 1).map(&:user_id))
+    @users = User.where(id: UserRole.where(role_id: 1).map(&:user_id))
     json_response 'Usuários', true, { users: @users }, :ok
   end
 end
